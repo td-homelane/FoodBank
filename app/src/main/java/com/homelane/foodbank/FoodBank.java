e99ebfddb5a3e959db47b2ca7309cd33163c6a02
Rajesh CP <rajesh.c@homelane.com>
2015-08-29 15:10:59 +0530

    Added application file

---
+++ app/src/main/java/com/homelane/foodbank/FoodBank.java
@@ -0,0 +1,24 @@
+package com.homelane.foodbank;
+
+import com.hl.hlcorelib.HLApplication;
+
+/**
+ * Created by hl0204 on 29/8/15.
+ */
+public class FoodBank extends HLApplication{
+
+    /**
+     * Called when the application is starting, before any activity, service,
+     * or receiver objects (excluding content providers) have been created.
+     * Implementations should be as quick as possible (for example using
+     * lazy initialization of state) since the time spent in this function
+     * directly impacts the performance of starting the first activity,
+     * service, or receiver in a process.
+     * If you override this method, be sure to call super.onCreate().
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
